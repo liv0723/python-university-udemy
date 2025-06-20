cc7b6fcd8e6b73a61d5c70adc26eea450bd474ff
yan pizarro <pizarroestevez2@gmail.como>
2025-06-20 16:50:10 -0300

    Assignment operators

---
+++ section_6/assignment_operators.py
@@ -0,0 +1,24 @@
+#Assignment Operator
+
+'''
+assignment operator   (=)
+'''
+
+name = input("Intro the Name: ")
+
+#assignment multiply
+var_1, var_2, var_3 = 1, 2.0, 'pepe'
+note_1, note2 = input('first note: '), input("second note: ")
+
+#assignment string
+age_1 = age_2 = input("Intro the same value of age: ")
+
+print ()
+print("********* Assignment Operators **********")
+print(f"The name is: {name}")
+print(f'The vars are using the assignment multiply: ', var_1, var_2, var_3, sep= " ")
+print(f'The notes are using the assignment multiply: {note_1}, {note2}')
+print(f'The age are using assignment string: {age_1} {age_2} ')
+
+
+
