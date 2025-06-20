df5da5de50eef4a3bc3d38ff31e74c65a938b419
yan pizarro <pizarroestevez2@gmail.como>
2025-06-20 14:30:40 -0300

    add the section 2

---
+++ section_2/exercise_presentation_7.py
@@ -0,0 +1,5 @@
+#Exersices
+
+print("Name: Yan Pizarro")
+print('Age: 36')
+print("""Country: Cuba""")
---
+++ section_2/hello_world.py
@@ -0,0 +1,6 @@
+# Program of hello world
+
+print("Hello World with Python")
+print('A String with comillas double')
+print("""A String with comillas triple""")
+
