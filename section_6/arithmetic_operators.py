102c0676d620b69a46950acf5292c8cd2491ba8a
yan pizarro <pizarroestevez2@gmail.como>
2025-06-20 14:58:55 -0300

    Arithmetics Operators

---
+++ section_6/arithmetic_operators.py
@@ -0,0 +1,30 @@
+#Arithmetics operators
+
+""""
+sum      (+)
+rest     (-)
+div      (/)
+div int  (//)
+mul      (*)
+mod      (%)
+exp      (**)
+"""
+
+sum_result = 2 + 2
+res_result = 2 - 2
+div_result = 4 / 2
+div_int_result = 5 // 2
+mul_result = 2 * 2
+mod_result = 5 % 2
+exp_result = 2 ** 3
+
+print()
+print('********** operations with Arithmetics Operators *********')
+print(f'sum : {sum_result}')
+print(f'res: {res_result}')
+print(f'div: {div_result}')
+print(f'div_int: {div_int_result}')
+print(f'mul: {mul_result}')
+print(f'mod: {mod_result}')
+print(f'exp: {exp_result}')
+
