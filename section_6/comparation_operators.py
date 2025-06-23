fb362ac95140d2586ed8e718c8222733c5d0ce57
yan pizarro <pizarroestevez2@gmail.como>
2025-06-23 10:57:59 -0300

    Comparation Operation

---
+++ section_6/comparation_operators.py
@@ -0,0 +1,21 @@
+#Comparation Operators
+
+#Equalt operator ==
+#distint operator !=
+#less operator <
+#less equal operator <=
+#mayor operator >
+#mayor equal operator >=
+
+a, b = 7, 5
+
+print(f'a: {a} b: {b}')
+print(f'Comparison a == b: {a == b} ')
+print(f'Comparison a != b: {a != b}')
+print(f'Comparison a > b: {a > b}')
+print(f'Comparison a >= b: {a >= b}')
+print(f'Comparison a < b: {a < b}')
+print(f'Comparison <= b: {a <= b}')
+
+
+
