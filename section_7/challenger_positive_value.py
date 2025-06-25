3cf25e3ecd8b5d79003cab0e1709208ca9770aa1
yan pizarro <pizarroestevez2@gmail.como>
2025-06-25 11:24:24 -0300

    Challenger Positive Value

---
+++ section_7/challenger_positive_value.py
@@ -0,0 +1,13 @@
+#Challenger Positive Value
+
+print("************** Challenger Positive Value **************")
+value = int(input("Intro the value: "))
+
+if value > 0:
+    print(f"The value {value} is Positive")
+elif value < 0:
+    print(f'The value {value} is Negative')
+else:
+    print(f'The value {value} is Cero')
+
+
