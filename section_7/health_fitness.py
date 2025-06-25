bc97efb3c3288fd7ad57e55c8bc5af230c373b83
yan pizarro <pizarroestevez2@gmail.como>
2025-06-25 14:20:50 -0300

    Health and Fitness

---
+++ section_7/health_fitness.py
@@ -0,0 +1,15 @@
+#Health and fitness
+
+print("********** Health and Fitness **********")
+
+DAILY_STEPS_GOAL = 10000
+CALORIES_DAILY_STEPS = 0.04
+
+user_name = input("Intro the name of user: ")
+daily_steps = int(input("Intro the steps daily: "))
+
+burn_calories = daily_steps * CALORIES_DAILY_STEPS
+
+it_was_fulfilled = "was fulfilled" if daily_steps > DAILY_STEPS_GOAL else 'wasn\'t fulfilled '
+
+print(f'The user: {user_name}\n',it_was_fulfilled, f'the steps are: {daily_steps}\n',f'and calorie: {burn_calories}', sep= ' ')
