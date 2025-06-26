3ca11401699c8d459d7600e29aad3e2e03f0daa6
yan pizarro <pizarroestevez2@gmail.como>
2025-06-26 12:38:43 -0300

    Mayor Two Number

---
+++ section_7/Mayor_two_number.py
@@ -0,0 +1,17 @@
+#Mayor two Numbers
+
+print("**************** Mayor two Number **************")
+
+number_1 = int(input("Intro the first of number to compare: "))
+number_2 = int(input("Intro the second of number to compare: "))
+
+if number_1 == number_2:
+    print(f"The numbers are equals first:{number_1} and secoond: {number_2}")
+elif number_1 > number_2:
+    print(f"The first number: {number_1} is mayor to the second number: {number_2}")
+else:
+    print(f"The second number: {number_2} is mayor to the first number: {number_1}")
+
+
+
+
